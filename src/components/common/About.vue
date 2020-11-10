71ab06809d1defd050c809014d4e2861c2d95491
kensuke goto <kensuke.goto.1981@gmail.com>
2020-11-10 20:17:51 +0900

    追加：部員ページ・タイトルとリード設定（共通コンポーネント作成）

---
+++ src/components/common/About.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="about">
+    <p class="title">{{ title }}</p>
+    <h1 class="text">{{ descr }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  props:{
+    title: {
+      type: String,
+      required: true
+    },
+    descr: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.about{
+
+  padding-right: 12px;
+  padding-left: 12px;
+  @include media(m){
+    width: 700px;
+    margin-right: auto;
+    margin-left: auto;
+    padding: 0;
+  }
+  .title{
+    line-height: 1;
+    font-size: 2rem;
+    font-weight: bold;
+    text-align: center;
+    @include media(m){
+      font-size: 2.4rem;
+    }
+  }
+  .text{
+    margin-top: 20px;
+    font-size: 1.4rem;
+    font-weight: normal;
+    line-height: 1.6;
+    @include media(m){
+      font-size: 1.6rem;
+      margin-top: 32px;
+    }
+  }
+}
+</style>
---
+++ src/views/Member.vue
@@ -0,0 +1,78 @@
+<template>
+  <PankuzuList />
+  <main>
+    <About :title="title" :descr="descr" />
+    <section class="memberList">
+      <div class="card-box__outer">
+        <ul class="card-box">
+          <!-- <li class="item" v-for="(item,key) of state.list" :key="key">
+            <NewsListItem :attr="item" />
+          </li> -->
+        </ul>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+
+import PankuzuList from "@/components/common/PankuzuList";
+import About from "@/components/common/About";
+// import axios from "axios"
+
+// import { computed, reactive } from 'vue';
+
+export default {
+  components:{
+    PankuzuList,
+    About
+    // bList
+  },
+  setup(){
+    return {
+      title: 'メンバー',
+      descr: 'ペルシャは、ネコの品種の一つ。ペルシャ猫。イラン原産。 長毛種の代表的な品種で、古くからショーキャットとして認められる品種の一つである。'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+main {
+  padding: 28px 12px 32px;
+  @include media(m){
+    padding: 36px 0 80px;
+  }
+}
+.memberList {
+  background-color: $yellow;
+  margin-top: 32px;
+  padding: 28px 8px 32px;
+  @include media(m){
+    margin-top: 36px;
+    padding: 36px 0 36px;
+  }
+}
+
+.card-box{
+  &__outer{
+    background-color: $white;
+    padding: 0 12px 32px;
+    margin-top: 20px;
+    @include media(m){
+      width: 960px;
+      margin: 24px auto 0;
+      padding-bottom: 40px;
+    }
+  }
+  .item {
+
+    padding-top: 12px;
+    padding-bottom: 12px;
+    border-bottom: 1px solid $grey;
+    @include media(m){
+      padding-top: 32px;
+      padding-bottom: 20px;
+    }
+  }
+}
+</style>
