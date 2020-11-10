947af33a25a25eb7027fbc803a11d708a80f453e
kensuke goto <kensuke.goto.1981@gmail.com>
2020-11-10 21:08:18 +0900

    追加：部員一覧を表示

--- src/views/Member.vue
+++ src/views/Member.vue
@@ -5,9 +5,10 @@
     <section class="memberList">
       <div class="card-box__outer">
         <ul class="card-box">
-          <!-- <li class="item" v-for="(item,key) of state.list" :key="key">
-            <NewsListItem :attr="item" />
-          </li> -->
+          <li class="item" v-for="(item,key) of state.list" :key="key">
+            <a class="item__image"><img :src="`/image/${item.image}`" alt=""></a>
+            <p class="item__title"><a>{{ item.label }}</a></p>
+          </li>
         </ul>
       </div>
     </section>
@@ -18,8 +19,8 @@
 
 import PankuzuList from "@/components/common/PankuzuList";
 import About from "@/components/common/About";
-// import axios from "axios"
-
+import axios from "axios"
+import { reactive } from 'vue';
 // import { computed, reactive } from 'vue';
 
 export default {
@@ -29,9 +30,23 @@ export default {
     // bList
   },
   setup(){
+
+    const state = reactive({
+      list: []
+    });
+
+    axios.get('/data/member.json')
+      .then((response) => {
+        state.list = response.data
+      })
+      .catch((e) => {
+        alert(e);
+      });
+
     return {
+      state,
       title: 'メンバー',
-      descr: 'ペルシャは、ネコの品種の一つ。ペルシャ猫。イラン原産。 長毛種の代表的な品種で、古くからショーキャットとして認められる品種の一つである。'
+      descr: 'ペルシャは、ネコの品種の一つ。ペルシャ猫。イラン原産。 長毛種の代表的な品種で、古くからショーキャットとして認められる品種の一つである。',
     }
   }
 }
@@ -54,6 +69,7 @@ main {
 }
 
 .card-box{
+
   &__outer{
     background-color: $white;
     padding: 0 12px 32px;
@@ -64,15 +80,44 @@ main {
       padding-bottom: 40px;
     }
   }
+
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+
   .item {
 
-    padding-top: 12px;
-    padding-bottom: 12px;
+    width: 50%;
+    padding-top: 28px;
+    padding-bottom: 28px;
     border-bottom: 1px solid $grey;
+    text-align: center;
     @include media(m){
+      width: 20%;
       padding-top: 32px;
-      padding-bottom: 20px;
+      padding-bottom: 32px;
     }
+
+    &__image{
+      width: 80%;
+      display: inline-block;
+      @include media(m){
+        width: 160px;
+      }
+    }
+
+    &__title{
+      margin-top: 28px;
+      font-weight: bold;
+      font-size: 1.4rem;
+      @include media(m){
+        margin-top: 32px;
+        font-size: 1.6rem;
+      }
+    }
+
+
+
   }
 }
 </style>
