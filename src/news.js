a8be5312bbb61378f3b7833ad2937ce7aa850bc4
kensuke goto <kensuke.goto.1981@gmail.com>
2020-11-18 16:53:31 +0900

    追加：記事の箱のスタイル

--- src/news.js
+++ src/news.js
@@ -1,9 +1,12 @@
 require('@/assets/scss/base.scss')
+require('@/assets/scss/news.scss')
 import { createApp } from 'vue'
 import router from './router'
 import TheHeader from "@/components/common/TheHeader";
+import PankuzuList from "@/components/common/PankuzuList";
 import TheFooter from "@/components/common/TheFooter";
 
 createApp(TheHeader).provide('conf', { isChild : true }).use(router).mount('#header');
+createApp(PankuzuList).use(router).mount('#pankuzu');
 
 createApp(TheFooter).mount('#footer')
